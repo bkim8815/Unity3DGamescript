5b219707fab011320adbcce87a773f09f5937440
Brian Kim <bkim8815@gmail.com>
2016-04-02 21:58:40 -0400

    saving all control engines

---
+++ PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : ShipController
+{
+	// Update is called once per frame
+	void Update ()
+	{
+
+
+
+		float requestedAcceleration = Input.GetAxis("Vertical");
+
+		if (requestedAcceleration > 0)
+		{
+			speed += acceleration * requestedAcceleration;
+		}
+		else
+		{
+			speed += deceleration * requestedAcceleration;
+		}
+
+		// Clamp
+		speed = Mathf.Clamp(speed, 0, maxSpeed);
+
+		transform.position += transform.forward * speed * Time.deltaTime;
+
+
+
+
+
+
+
+		_rotationDelta.x = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+		_rotationDelta.z = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+
+		transform.Rotate(_rotationDelta);
+	}
+
+
+	void FixedUpdate ()
+	{
+		if (Input.GetButton("Fire1"))
+		{
+			FireLaser();
+		}
+	}
+
+
+	public override void Die ()
+	{
+		GameController.Instance.Lose();
+	}
+
+
+}
